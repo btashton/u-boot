missimer/quest-edison-uboot

sfi: add sfi support
JIRA: EDISON-316

Change-Id: I0dc81365fd26fc35fa940e06b893b81dfde6ee40
Signed-off-by: scolleux <sebastienx.colleur@intel.com>
Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

arch/x86/include/asm/sfi.h
@@ -1,0 +1,117 @@
+/* Copyright (c) 2012 Intel
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __INTEL_SFI_H
+#define __INTEL_SFI_H
+
+#include <asm/arch/sysinfo.h>
+
+/* Memory type definitions */
+enum sfi_mem_type {
+	SFI_MEM_RESERVED,
+	SFI_LOADER_CODE,
+	SFI_LOADER_DATA,
+	SFI_BOOT_SERVICE_CODE,
+	SFI_BOOT_SERVICE_DATA,
+	SFI_RUNTIME_SERVICE_CODE,
+	SFI_RUNTIME_SERVICE_DATA,
+	SFI_MEM_CONV,
+	SFI_MEM_UNUSABLE,
+	SFI_ACPI_RECLAIM,
+	SFI_ACPI_NVS,
+	SFI_MEM_MMIO,
+	SFI_MEM_IOPORT,
+	SFI_PAL_CODE,
+	SFI_MEM_TYPEMAX,
+};
+
+#define SFI_SYST_MAGIC 0x54535953
+#define SFI_MMAP_MAGIC 0x50414d4d
+
+struct sfi_mem_entry {
+	enum sfi_mem_type type;
+	u64 phy_start;
+	u64 vir_start;
+	u64 pages;
+	u64 attrib;
+}__attribute__((packed));
+
+struct sfi_apic_table_entry {
+	u64 phys_addr;  /* phy base addr for APIC reg */
+}__attribute__((packed));
+
+struct sfi_timer_table_entry {
+	u64 phys_addr;  /* phy base addr for the timer */
+	u32 freq_hz;    /* in HZ */
+	u32 irq;
+}__attribute__((packed));
+
+struct sfi_table_header {
+	char signature[4];
+	u32 length;
+	u8 revision;
+	u8 checksum;
+	char oem_id[6];
+	char oem_table_id[8];
+}__attribute__((packed));
+
+struct sfi_quad_word {
+	u32 low;
+	u32 high;
+};
+
+struct sfi_table {
+	struct sfi_table_header header;
+	union {
+		u64 pentry[1];
+		struct sfi_quad_word entry[1];
+	};
+}__attribute__((packed));
+
+#define SFI_TBL_HEADER_LEN      24
+
+#define SFI_GET_NUM_ENTRIES(ptable, entry_type) \
+	((ptable->header.length - sizeof(struct sfi_table_header)) / \
+	(sizeof(entry_type)))
+
+#define SFI_GET_ENTRY_NUM(ptable, entry) \
+	((ptable->header.length - SFI_TBL_HEADER_LEN) / \
+	(sizeof(struct entry)))
+
+#define SFI_BASE_ADDR		0x000E0000
+#define SFI_LENGTH		0x00020000
+
+extern int sfi_mtimer_num;
+
+void sfi_parse_mtmr(void);
+struct sfi_timer_table_entry *sfi_get_mtmr(int hint);
+void fill_memranges_from_e820(struct sysinfo_t *info);
+
+#ifdef CONFIG_X86_IO_APIC
+int sfi_parse_ioapic(void);
+#endif /* CONFIG_X86_IO_APIC */
+
+#ifdef CONFIG_DETECT_RAM_SIZE_WORKAROUND
+unsigned long long sfi_get_max_usable_ram(struct sysinfo_t *info);
+#endif
+
+#ifdef CONFIG_SFI
+extern phys_size_t sfi_get_ram_size(void);
+#endif
+
+#endif /* __SFI_H__ */

btashton/u-boot

arch/x86/include/asm/sfi.h
@@ -1,137 +1,119 @@
-/*
- * Copyright(c) 2009 Intel Corporation. All rights reserved.
+/* Copyright (c) 2012 Intel 
  *
- * SPDX-License-Identifier:	GPL-2.0+	BSD-3-Clause
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
 
-#ifndef _LINUX_SFI_H
-#define _LINUX_SFI_H
+#ifndef _SFI_H
+#define _SFI_H
 
-#include <errno.h>
-#include <linux/types.h>
+#include <asm/arch/sysinfo.h>
+#include <asm/e820.h>
 
-/* Table signatures reserved by the SFI specification */
-#define SFI_SIG_SYST		"SYST"
-#define SFI_SIG_FREQ		"FREQ"
-#define SFI_SIG_CPUS		"CPUS"
-#define SFI_SIG_MTMR		"MTMR"
-#define SFI_SIG_MRTC		"MRTC"
-#define SFI_SIG_MMAP		"MMAP"
-#define SFI_SIG_APIC		"APIC"
-#define SFI_SIG_XSDT		"XSDT"
-#define SFI_SIG_WAKE		"WAKE"
-#define SFI_SIG_DEVS		"DEVS"
-#define SFI_SIG_GPIO		"GPIO"
+/* Memory type definitions */
+enum sfi_mem_type {
+	SFI_MEM_RESERVED,
+	SFI_LOADER_CODE,
+	SFI_LOADER_DATA,
+	SFI_BOOT_SERVICE_CODE,
+	SFI_BOOT_SERVICE_DATA,
+	SFI_RUNTIME_SERVICE_CODE,
+	SFI_RUNTIME_SERVICE_DATA,
+	SFI_MEM_CONV,
+	SFI_MEM_UNUSABLE,
+	SFI_ACPI_RECLAIM,
+	SFI_ACPI_NVS,
+	SFI_MEM_MMIO,
+	SFI_MEM_IOPORT,
+	SFI_PAL_CODE,
+	SFI_MEM_TYPEMAX,
+};
 
-#define SFI_SIGNATURE_SIZE	4
-#define SFI_OEM_ID_SIZE		6
-#define SFI_OEM_TABLE_ID_SIZE	8
+#define SFI_SYST_MAGIC 0x54535953
+#define SFI_MMAP_MAGIC 0x50414d4d
 
-#define SFI_NAME_LEN		16
-#define SFI_TABLE_MAX_ENTRIES	16
+struct sfi_mem_entry {
+	enum sfi_mem_type type;
+	u64 phy_start;
+	u64 vir_start;
+	u64 pages;
+	u64 attrib;
+} __attribute__ ((packed));
+
+struct sfi_apic_table_entry {
+	u64 phys_addr;		/* phy base addr for APIC reg */
+} __attribute__ ((packed));
+
+struct sfi_timer_table_entry {
+	u64 phys_addr;		/* phy base addr for the timer */
+	u32 freq_hz;		/* in HZ */
+	u32 irq;
+} __attribute__ ((packed));
+
+struct sfi_table_header {
+	char signature[4];
+	u32 length;
+	u8 revision;
+	u8 checksum;
+	char oem_id[6];
+	char oem_table_id[8];
+} __attribute__ ((packed));
+
+struct sfi_quad_word {
+	u32 low;
+	u32 high;
+};
+
+struct sfi_table {
+	struct sfi_table_header header;
+	union {
+		u64 pentry[1];
+		struct sfi_quad_word entry[1];
+	};
+} __attribute__ ((packed));
+
+#define SFI_TBL_HEADER_LEN      24
 
 #define SFI_GET_NUM_ENTRIES(ptable, entry_type) \
-	((ptable->header.len - sizeof(struct sfi_table_header)) / \
+	((ptable->header.length - sizeof(struct sfi_table_header)) / \
 	(sizeof(entry_type)))
-/*
- * Table structures must be byte-packed to match the SFI specification,
- * as they are provided by the BIOS.
- */
-struct __packed sfi_table_header {
-	char	sig[SFI_SIGNATURE_SIZE];
-	u32	len;
-	u8	rev;
-	u8	csum;
-	char	oem_id[SFI_OEM_ID_SIZE];
-	char	oem_table_id[SFI_OEM_TABLE_ID_SIZE];
-};
 
-struct __packed sfi_table_simple {
-	struct sfi_table_header		header;
-	u64				pentry[1];
-};
+#define SFI_GET_ENTRY_NUM(ptable, entry) \
+	((ptable->header.length - SFI_TBL_HEADER_LEN) / \
+	(sizeof(struct entry)))
 
-/* Comply with UEFI spec 2.1 */
-struct __packed sfi_mem_entry {
-	u32	type;
-	u64	phys_start;
-	u64	virt_start;
-	u64	pages;
-	u64	attrib;
-};
+#define SFI_BASE_ADDR		0x000E0000
+#define SFI_LENGTH		0x00020000
 
-struct __packed sfi_cpu_table_entry {
-	u32	apic_id;
-};
+extern int sfi_mtimer_num;
 
-struct __packed sfi_cstate_table_entry {
-	u32	hint;		/* MWAIT hint */
-	u32	latency;	/* latency in ms */
-};
+void sfi_parse_mtmr(void);
+struct sfi_timer_table_entry *sfi_get_mtmr(int hint);
+void fill_memranges_from_e820(struct sysinfo_t *info);
 
-struct __packed sfi_apic_table_entry {
-	u64	phys_addr;	/* phy base addr for APIC reg */
-};
+#ifdef CONFIG_X86_IO_APIC
+int sfi_parse_ioapic(void);
+#endif /* CONFIG_X86_IO_APIC */
 
-struct __packed sfi_freq_table_entry {
-	u32	freq_mhz;	/* in MHZ */
-	u32	latency;	/* transition latency in ms */
-	u32	ctrl_val;	/* value to write to PERF_CTL */
-};
+#ifdef CONFIG_DETECT_RAM_SIZE_WORKAROUND
+unsigned long long sfi_get_max_usable_ram(struct sysinfo_t *info);
+#endif
 
-struct __packed sfi_wake_table_entry {
-	u64	phys_addr;	/* pointer to where the wake vector locates */
-};
+#ifdef CONFIG_SFI
+extern phys_size_t sfi_get_ram_size(void);
+extern unsigned sfi_setup_e820(unsigned max_entries, struct e820entry *entries);
+#endif
 
-struct __packed sfi_timer_table_entry {
-	u64	phys_addr;	/* phy base addr for the timer */
-	u32	freq_hz;	/* in HZ */
-	u32	irq;
-};
-
-struct __packed sfi_rtc_table_entry {
-	u64	phys_addr;	/* phy base addr for the RTC */
-	u32	irq;
-};
-
-struct __packed sfi_device_table_entry {
-	u8	type;		/* bus type, I2C, SPI or ...*/
-	u8	host_num;	/* attached to host 0, 1...*/
-	u16	addr;
-	u8	irq;
-	u32	max_freq;
-	char	name[SFI_NAME_LEN];
-};
-
-enum {
-	SFI_DEV_TYPE_SPI	= 0,
-	SFI_DEV_TYPE_I2C,
-	SFI_DEV_TYPE_UART,
-	SFI_DEV_TYPE_HSI,
-	SFI_DEV_TYPE_IPC,
-	SFI_DEV_TYPE_SD,
-};
-
-struct __packed sfi_gpio_table_entry {
-	char	controller_name[SFI_NAME_LEN];
-	u16	pin_no;
-	char	pin_name[SFI_NAME_LEN];
-};
-
-struct sfi_xsdt_header {
-	uint32_t oem_revision;
-	uint32_t creator_id;
-	uint32_t creator_revision;
-};
-
-typedef int (*sfi_table_handler) (struct sfi_table_header *table);
-
-/**
- * write_sfi_table() - Write Simple Firmware Interface tables
- *
- * @base:	Address to write table to
- * @return address to use for the next table
- */
-u32 write_sfi_table(u32 base);
-
-#endif /*_LINUX_SFI_H */
+#endif /* _SFI_H */